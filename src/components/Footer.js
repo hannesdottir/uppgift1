a86a8f03176d5fa35f45636bb49bea37ed07ef69
Tobias Jakobsson <tobias.jakobsson@espresso-house.se>
2023-02-27 21:14:33 +0100

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className='bg-white text-center m-6'>
-            <p className='font-light text-sm tracking-wide'>&copy; 2035 by BizBud. Skapad med <span className='underline'>Wix.com</span></p>
+            <p className='font-light text-sm tracking-wide'>&copy; 2045 by BizBud. Skapad med <span className='underline'>Wix.com</span></p>
         </div>
     )
 }
