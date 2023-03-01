cda7c5dadf62f44e080e5fb7c4f22ed0b26bef22
Tobias Jakobsson <tobias.jakobsson@espresso-house.se>
2023-03-01 20:15:25 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 import './styles/style.css'
-import NavMenu from './components/NavMenu';
-import Banner from './components/Banner';
-import Services from './components/Services';
-import Numbers from './components/Numbers';
-import About from './components/About';
-import ClientReviews from './components/ClientReviews';
-import Contact from './components/Contact';
-import Map from './components/Map';
-import Footer from './components/Footer';
-import CompanyMentions from './components/CompanyMentions';
-import CookieBar from './components/CookieBar';
+import NavMenu from './components/NavMenu'
+import Banner from './components/Banner'
+import Services from './components/Services'
+import Numbers from './components/Numbers'
+import About from './components/About'
+import ClientReviews from './components/ClientReviews'
+import Contact from './components/Contact'
+import Map from './components/Map'
+import Footer from './components/Footer'
+import CompanyMentions from './components/CompanyMentions'
+import CookieBar from './components/CookieBar'
 
 function App() {
 
