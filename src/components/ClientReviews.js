bef3144831a8b1daca268895b70a4f8333b382fc
Tobias Jakobsson <tobias.jakobsson@espresso-house.se>
2023-03-03 13:22:14 +0100

    .

--- src/components/ClientReviews.js
+++ src/components/ClientReviews.js
@@ -4,7 +4,7 @@ export default function ClientCarousel() {
     return (
         <div className='bg-violet-600 text-center text-white p-16'>
             <p className='font-bold tracking-widest'>KUNDCITAT</p>
-            <p className='text-5xl font-semibold mt-12'>“Det här är ett omdöme.</p>
+            <p className='text-5xl font-semibold mt-12'>“Det här är ett omdöme. </p>
             <p className='text-5xl font-semibold mb-24'> Klicka  för att redigera.”</p>
             <p className='font-bold'>Jens Persson</p>
             <p>Företagsnamn</p>
